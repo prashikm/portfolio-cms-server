8367ef25bbfcaa3eaffac238887149b296ed76e9
prashikm <prashikm861@gmail.com>
2024-07-06 13:27:18 +0530

    update DEBUG

--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["*"]
 
